818ab2b863e32440852358e78ef68346d62d1f0b
Kate Tester <katemorris@gmail.com>
2020-09-07 14:02:36 -0600

    Adds methods to run a turn.

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,5 @@
+require './lib/turn'
+
 class Round
   attr_reader :deck, :turns
 
@@ -8,5 +10,21 @@ class Round
 
   def current_card
     @deck.cards.first
+  end
+
+  def change_card
+    @deck.cards.shift
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, change_card)
+    @turns << turn
+    turn
+  end
+
+  def number_correct
+    @turns.select do |turn|
+      turn.correct?
+    end.count
   end
 end
